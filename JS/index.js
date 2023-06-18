4c6fb97c9db4a1f5e9b71171a9cbebfdc9bf0791
cavermatthew <mrbonner@uw.edu>
2023-06-17 17:03:13 -0700

    Found Camera Install Page

--- JS/index.js
+++ JS/index.js
@@ -83,6 +83,7 @@ const headerHTML = ` \
                 <div class="submenublock"><a href="${path_origin}/Pages/Research Profiles/RainOnSnowModule.html">Rain on Snow Module</a></div> \
                 <div class="submenublock"><a href="https://www.zooniverse.org/projects/mozerm/snow-spotter">Citizen Science</a></div> \
                 <div class="submenublock"><a href="${path_origin}/Pages/FreeUsePhotos.html">Free Use Snow Photos</a></div> \
+                <div class="submenublock"><a href="${path_origin}/Pages/Archived Pages/CameraInTrees.html">Time Lapse Camera Install</a></div> \
                 <div class="submenublock"><a href="${path_origin}/Pages/Join Us Pages/joinUsUndergrad.html">Join Us! Undergraduates</a></div> \
                 <div class="submenublock"><a href="${path_origin}/Pages/Join Us Pages/joinUsGraduate.html">Join Us! Graduates</a></div> \
                 <div class="submenublock"><a href="${path_origin}/Pages/Join Us Pages/joinUsPostDoc.html">Join Us! Post-Doctorals</a></div> \
